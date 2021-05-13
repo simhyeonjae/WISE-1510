a4e2735f504fba9d62b5bb32a1bf0bfc38fa7ef1
simhj316 <55834370+simhj316@users.noreply.github.com>
2021-05-13 10:26:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -295,7 +295,7 @@ int  MGGetPercentage(float volts, float *pcurve)
       return -1;
    } else {
       //return pow(10, ((volts*1000/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
-	   return pow(10, (((volts)/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
+	   return pow(10, (((volts*1.5)/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
    }
 }
 
