cee1b5e97f441d1c22f5a2534d111e0e4e0a30e6
simhj316 <55834370+simhj316@users.noreply.github.com>
2021-05-24 19:39:02 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -282,11 +282,11 @@ float MGRead(void)
     int i;
     float v=0;
 
-   /* for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    for (i=0;i<READ_SAMPLE_TIMES;i++) {
         v += ain;
         // delay(READ_SAMPLE_INTERVAL);
         Thread::wait(1000);        
-    }*/
+    }
 	
     //v = (v/READ_SAMPLE_TIMES) * 5 /1024 ;
    v = (v/READ_SAMPLE_TIMES) * 3.42 ;
