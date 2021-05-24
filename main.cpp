316596e25387f28f977aaa90c0f4ef1eddf3aef7
simhj316 <55834370+simhj316@users.noreply.github.com>
2021-05-24 19:13:58 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -274,10 +274,10 @@ float voltageConversion(int value){
 float getRatio(){
   int value = ain;
   float v_out = voltageConversion(value);
-  //return (5.0 - v_out) / v_out;
+  return (5.0 - v_out) / v_out;
 	
 	
-	return (5.0 – v_out) / (v_out* 0.15);
+	//return (5.0 – v_out) / (v_out* 0.15);
 }
 
 float MGRead(void)
