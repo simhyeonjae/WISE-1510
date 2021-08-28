00d147ba56f61b90d1620aee536d0be5719d19af
izabel1209 <55544125+izabel1209@users.noreply.github.com>
2021-08-28 22:51:47 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -276,7 +276,7 @@ float MGRead(void)
     }
     //v = (v/READ_SAMPLE_TIMES) / 1024 * 5 ;
 	//v = (v/READ_SAMPLE_TIMES)/1024 * 5;
-	v =  (v/READ_SAMPLE_TIMES)*5/1024;
+	v =  (v/READ_SAMPLE_TIMES)*25000/1024;
     //v = (v/READ_SAMPLE_TIMES) *3.42 ;
     return v;
 }
@@ -292,16 +292,18 @@ Remarks: By using the slope and a point of the line. The x(logarithmic value of 
 ************************************************************************************/
 float  MGGetPercentage(float volts, float *pcurve)
 {
-   //if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
-     // return -1;
-   //} else {
-     // return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
-	   //return pow(volts,3.401);
+   if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
+     return -1;
+   } else {
+      return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
+   }
+	   
+	return pow(volts,3.401);
  	
 	
-	float dust=(volts-0.35) / 0.005;
-	return dust;
-	//}
+	//float dust=(volts-0.35) / 0.005;
+	//return dust;
+	
 }
 
 static unsigned int co2_sensor_sku_sen0159(void)
@@ -322,7 +324,7 @@ static unsigned int co2_sensor_sku_sen0159(void)
 	
     percentage = MGGetPercentage(volts,CO2Curve);
 	//percentage = MGGetPercentage(x,CO2Curve)*100;
-    NODE_DEBUG("Dust3_2:");
+    NODE_DEBUG("CO2:");
     if (percentage == -1) {
         NODE_DEBUG(" <400 ");
     } else {
