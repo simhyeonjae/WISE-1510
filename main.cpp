24a8eb9aa5bb0c54626c731417da7ec799bdc58b
izabel1209 <55544125+izabel1209@users.noreply.github.com>
2021-09-03 11:44:55 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -324,7 +324,7 @@ static unsigned int co2_sensor_sku_sen0159(void)
 	
     percentage = MGGetPercentage(volts,CO2Curve);
 	//percentage = MGGetPercentage(x,CO2Curve)*100;
-    NODE_DEBUG("Dust3_1_3:");
+    NODE_DEBUG("Dust3_1_4:");
     if (percentage == -1) {
         NODE_DEBUG(" <400 ");
     } else {
