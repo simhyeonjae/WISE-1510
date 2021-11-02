82f088b5e1a7d901a76f093ea34b519f4710dec6
izabel1209 <55544125+izabel1209@users.noreply.github.com>
2021-11-02 21:54:17 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -276,7 +276,7 @@ float MGRead(void)
     }
     //v = (v/READ_SAMPLE_TIMES) / 1024 * 5 ;
 	//v = (v/READ_SAMPLE_TIMES)/1024 * 5;
-	v =  (v/READ_SAMPLE_TIMES)*4;
+	v =  (v/READ_SAMPLE_TIMES)*3.5;
     //v = (v/READ_SAMPLE_TIMES) *3.42 ;
     return v;
 }
